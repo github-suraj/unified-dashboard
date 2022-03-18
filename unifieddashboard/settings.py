c71ba8639aaa9eb0c2f245fbeaa1838fbcc1415e
github-suraj <surajkumarjaiswal053@gmail.com>
2022-03-18 22:50:14 +0530

    add allowed host for heruku

--- unifieddashboard/settings.py
+++ unifieddashboard/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = eval(os.environ['DEBUG'])
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['.herokuapp.com', '.researchthroughdesign.org']
 
 
 # Application definition
